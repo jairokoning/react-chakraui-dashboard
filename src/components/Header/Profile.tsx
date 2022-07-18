20d4738b2f85002dfe784da74a25d12157aabdbd
Jairo Koning <jairo.koning@gmail.com>
2022-07-17 22:11:52 -0300

    feat: add media query to header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,12 +1,18 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
-  return (
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
+  return (    
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Jairo Koning</Text>
-        <Text color="gray.300" fontSize="small">jairo.koning@gmail.com</Text>            
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Jairo Koning</Text>
+          <Text color="gray.300" fontSize="small">jairo.koning@gmail.com</Text>            
+        </Box>
+      )}
       <Avatar size="md" name="Jairo Koning" src="https://github.com/jairokoning.png" />
     </Flex>
   )
