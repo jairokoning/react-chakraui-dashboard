524521e15ba16451764baa20d9e2cde49d370df8
Jairo Koning <jairo.koning@gmail.com>
2022-07-20 07:47:00 -0300

    feat: add media query to users list

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,10 +1,15 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+  const isWideScreen = useBreakpointValue({
+    base: false,
+    lg: true
+  })
+  
   return (
     <Box>
       <Header />
@@ -29,13 +34,13 @@ export default function UserList() {
                   <Checkbox colorScheme="pink" />
                 </Th>
                 <Th>Usuário</Th>
-                <Th>Data de Cadastro</Th>
+                { isWideScreen && <Th>Data de Cadastro</Th> }
                 <Th width="8"></Th>
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
-                <Td px="6">
+                <Td px={["4","4","6"]}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -44,20 +49,24 @@ export default function UserList() {
                     <Text fontSize="sm" color="gray.300">jairo.koning@gmail.com</Text>
                   </Box>
                 </Td>
-                <Td>10 de Julho, 2022</Td>
+                { isWideScreen && <Td>10 de Julho, 2022</Td> }
                 <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    Criar Novo
-                  </Button>
+                  { isWideScreen && 
+                      
+                    <Button
+                      as="a"
+                      size="sm"
+                      colorScheme="purple"
+                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                    >
+                      Editar
+                    </Button>
+                    
+                  }
                 </Td>
               </Tr>
               <Tr>
-                <Td px="6">
+                <Td px={["4","4","6"]}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -66,20 +75,25 @@ export default function UserList() {
                     <Text fontSize="sm" color="gray.300">jairo.koning@gmail.com</Text>
                   </Box>
                 </Td>
-                <Td>10 de Julho, 2022</Td>
+                { isWideScreen && <Td>10 de Julho, 2022</Td> }
                 <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    Criar Novo
-                  </Button>
+                  { isWideScreen && 
+                    
+                      <Button
+                        as="a"
+                        size="sm"
+                        colorScheme="purple"
+                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                      >
+                        Editar
+                      </Button>
+                    
+                  }
                 </Td>
+                
               </Tr>
               <Tr>
-                <Td px="6">
+                <Td px={["4","4","6"]}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -88,16 +102,20 @@ export default function UserList() {
                     <Text fontSize="sm" color="gray.300">jairo.koning@gmail.com</Text>
                   </Box>
                 </Td>
-                <Td>10 de Julho, 2022</Td>
+                { isWideScreen && <Td>10 de Julho, 2022</Td> }
                 <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    Criar Novo
-                  </Button>
+                  { isWideScreen && 
+                      
+                    <Button
+                      as="a"
+                      size="sm"
+                      colorScheme="purple"
+                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                    >
+                      Editar
+                    </Button>
+                      
+                  }
                 </Td>
               </Tr>
             </Tbody>
